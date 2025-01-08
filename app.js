042de33126f9b768c8a3003c9b060482eca3d122
HazyZen <vio.waqas@gmail.com>
2025-01-08 11:40:43 +0800

    Completed

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ document.body.insertBefore(gridBtn, container);
 
 const createGrid = (size) => {
   container.innerHTML = "";
+
   if (size < 100) {
     for (let i = 0; i < size; i++) {
       for (let j = 0; j < size; j++) {
@@ -17,11 +18,14 @@ const createGrid = (size) => {
         square.style.height = `${960 / size}` + "px";
 
         square.opacity = 0.1;
+        let r = Math.floor(Math.random() * 256);
+        let g = Math.floor(Math.random() * 256);
+        let b = Math.floor(Math.random() * 256);
 
         square.addEventListener("mouseenter", () => {
           if (square.opacity < 1) {
             square.opacity = Math.min(square.opacity + 0.1, 1);
-            square.style.backgroundColor = `rgba(0, 0, 0, ${square.opacity})`;
+            square.style.backgroundColor = `rgba(${r}, ${g}, ${b}, ${square.opacity})`;
           }
         });
       }
@@ -30,9 +34,11 @@ const createGrid = (size) => {
     newGrid();
   }
 };
+createGrid(16);
 
 const newGrid = () => {
   let size = prompt("How many boxes per side?");
   createGrid(size);
 };
+
 gridBtn.addEventListener("click", newGrid);
